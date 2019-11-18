da4ef47327561c4768e481526951e74fb4bd168f
maivu2054 <mai.vu@uef.fi>
2019-11-18 22:45:59 +0200

    seen

--- create_alc.R
+++ create_alc.R
@@ -1,56 +1,66 @@
-#Mai Vu 17 Nov 19
+#Mai Vu 13 Nov 19
 #read both student-mat.csv and student-por.csv into R 
+#First install.packages(c("dplyr"), ("ggplo2"))
 
-setwd(getwd)
-mat <- read.csv(D:/ONE DRIVE/OneDrive - University of Eastern Finland/Attachments/8 statistics/2 R program/r practice/IODS-project/data/"student-mat.csv", header = T, sep= ";")
-por <- read.csv()
-
+#Read both student-mat.csv and student-por.csv into R (from the data folder) and explore the structure and dimensions of the data
 url <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets"
 
-# web address for math class data
 url_math <- paste(url, "student-mat.csv", sep = "/")
 
-# print out the address
-url_math
-
-# read the math class questionaire data into memory
 math <- read.table(url_math, sep = ";" , header=TRUE)
 
-# web address for portuguese class data
-url_por <- paste(url, "student-por.csv", sep ="/ ")
-url_por
-# read the portuguese class questionaire data into memory
+url_por <- paste(url, "student-por.csv", sep = "/")
+
 por <- read.table(url_por, sep = ";", header = TRUE)
-View (por)
-# look at the column names of both data
-colnames(math)
 
+str(por)
 
-#explore the structure and dimensions of the data
-
-# alc is available
+str(math)
 
 # access the 'tidyverse' packages dplyr and ggplot2
 library(dplyr); library(ggplot2)
+#Join the two data sets using the variables "school", "sex", "age", "address", "famsize", "Pstatus", "Medu", "Fedu", "Mjob", "Fjob", "reason", "nursery","internet" as (student) identifiers. Keep only the students present in both data sets
 
-# define a new column alc_use by combining weekday and weekend alcohol use
-alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
-dim(alc)
-# initialize a plot of alcohol use
-g1 <- ggplot(data = alc, aes(x = alc_use))
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
 
-# define the plot as a bar plot and draw it
-g1 + geom_bar()
+math_por <- inner_join(math, por, by = join_by, suffix = c(".math", ".por"))
 
-# define a new logical column 'high_use'
-alc <- mutate(alc, high_use = alc_use > 2)
+glimpse(math_por)
 
-# initialize a plot of 'high_use'
-g2 <- ggplot(data = alc, aes (x=high_use), col=sex)
+str(math_por)
+"The new dataset has 382 observations and 53 variables"
 
-# draw a bar plot of high_use by sex
-g2 + geom_bar(aes (x= sex, y= high_use))
+#Removing duplicates
 
-gather(alc) %>% glimpse
-# draw a bar plot of each variable
-gather(alc) %>% ggplot(aes(value)) + facet_wrap("key", scales = "free")
+dat <- select(math_por, one_of(join_by))
+
+
+notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
+
+for(column_name in notjoined_columns) {
+  
+  two_columns <- select(math_por, starts_with(column_name))  
+  
+  first_column <- select(two_columns, 1)[[1]]
+  
+  
+  if(is.numeric(first_column)) { dat[column_name] <- round(rowMeans(two_columns))} else { dat[column_name] <- first_column }}
+
+glimpse(dat)  
+
+"With the duplicates removed the data now has 382 observations and 33 variables"
+
+#Take the average of the answers related to weekday and weekend alcohol consumption to create a new column 'alc_use' to the joined data.
+
+library(ggplot2)
+
+dat <- mutate(dat, alc_use = (Dalc + Walc) / 2)
+
+dat <- mutate(dat, high_use = alc_use > 2)
+
+glimpse(dat)
+
+#Save the joined and modified data set to the 'data' folder, using for example write.csv() or write.table() functions
+
+write.table(dat, file= "alc_use.csv")
+
