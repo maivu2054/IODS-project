5b4bdfb15a5bb4ddb158c65f05748897ced68367
maivu2054 <mai.vu@uef.fi>
2019-12-02 15:07:58 +0200

    upload chap5

--- create_human.R
+++ create_human.R
@@ -1,3 +1,6 @@
+dev.off()
+break()
+rm(list = ls())
 
 library(ggplot2)
 library(dplyr)
@@ -26,7 +29,7 @@ colnames(hd)
 
 #gii data
 colnames(gii)[3] <- "GII_index"
-colnames(gii)[4] <- "raternity_mort"
+colnames(gii)[4] <- "mater_mort"
 colnames(gii)[5] <- "birthrate"
 colnames(gii)[6] <- "repinparliament"
 colnames(gii)[7] <- "edu_F"
@@ -50,3 +53,55 @@ glimpse(human)
 head (human)
 
 write.table(human, file= "human.csv")
+
+# Date 1 Dec: Excercise 5
+
+# Q1: change GNI to numeric and data description
+install.packages("stringr")
+library(stringr)
+library(MASS)
+library(stringr)
+library (tidyr)
+library (dplyr)
+#structure of human and GNI
+str(human)
+str (human$GNI)
+human$GNI <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric()
+human$GNI%>%as.numeric(str_replace(human$GNI, pattern=",", replace =""))
+head(human[, 1:19], 4)
+
+#
+# Q2 
+"Excluding unneeded variables keep only the columns matching the following variable names: 
+Country, Edu2.FM, Labo.FM, Edu.Exp, Life.Exp, GNI, Mat.Mor, Ado.Birth, Parli.F"
+
+keep <- c("Country", "edu_ratio", "participation_ratio", "life_expect",
+          "y_of_edu", "GNI", "mater_mort", "birthrate", "repinparliament")
+
+# select the 'keep' columns
+human <- dplyr::select(human, one_of(keep))
+
+
+# 3 Dealing with not available (NA) values
+# print out a completeness indicator of the logical "completeness indicator" vector
+complete.cases(human)
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human))
+head (human_)
+str(human_)
+
+# 4 Removing observations relating to regions instead of countries
+# define the last indice we want to keep
+last <- nrow(human_) - 7
+# choose everything until the last 7 observations
+human_ <- human_[1:last,]
+
+# Defining the row names and removing country columns
+
+rownames(human_) <- human_$Country
+
+human_ <- dplyr::select(human_, -Country)
+
+str(human_)
+# The data should now have 155 observations and 8 variables
+write.table(human_, file= "human_.csv")
